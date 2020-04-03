388828e988f759c24347b5d2526c444fde245a8a
kevin <kevinkip77@gmail.com>
2020-04-03 10:19:09 +0900

    Update

--- phase vs broadwall height.py
+++ phase vs broadwall height.py
@@ -1,7 +1,7 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import math
 
-x = 2*math.pi/math.pow(10,8)
+x = (2*math.pi)/(3*math.pow(10,8))
 z = 0
 a = 8
 res = 40
@@ -11,9 +11,12 @@ zlist, alist, fc10list, betalist = ([] for i in range (4))
 while z <= 200:
     zlist.append(z)
     alist.append(a)
-    fc10 = 300/(2*a)
+    fc10 = (3*math.pow(10,11))/(2*a)
     fc10list.append(fc10)
+    beta = x*math.sqrt((math.pow(35000000000,2))-(math.pow(fc10,2)))
     betalist.append(beta)
     z = z + zinc
     a = a - adec
-print (fc10list,"\n", betalist)
+#print (fc10list,"\n", betalist)
+plt.plot(fc10, a)
+plt.show()
